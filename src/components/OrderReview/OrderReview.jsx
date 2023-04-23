31dbae4a1becef5c86b8f803ec6411525db1b4d6
snayeemu <snayeemu2@gmail.com>
2023-04-23 22:14:52 +0600

    first commit

---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const OrderReview = () => {
+  return (
+    <div>
+      <h2>Order Review Page</h2>
+    </div>
+  );
+};
+
+export default OrderReview;
